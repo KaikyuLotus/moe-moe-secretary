e535bcdd3653895a577c28a64d957a55bfa6859f
Kaikyu <kaikyu4k@gmail.com>
2020-01-25 22:29:04 +0100

    Github CI small improvements

--- .github/workflows/deployer.py
+++ .github/workflows/deployer.py
@@ -5,8 +5,10 @@ import json
 
 from telegram import Bot
 
-token = os.environ["bot_token"]
-target_chat_ids = json.loads(os.environ["chat_ids"])
+token = os.environ["BOT_TOKEN"]
+target_chat_ids = json.loads(os.environ["TARGET_CHAT_IDS"])
+actor = os.environ["GITHUB_ACTOR"]
+message = os.environ["COMMIT_MESSAGE"]
 
 print(f"Sending to the following chat IDs: {target_chat_ids}")
 
