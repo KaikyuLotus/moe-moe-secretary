4fc4daf8a4c166495297fff5acba6e2720555d0f
Kaikyu <kaikyu4k@gmail.com>
2020-01-25 22:45:44 +0100

    Fix for SHA7 in Github CI

--- .github/workflows/deployer.py
+++ .github/workflows/deployer.py
@@ -9,14 +9,14 @@ token = os.environ["BOT_TOKEN"]
 artifact = os.environ["ARTIFACT"]
 actor = os.environ["GITHUB_ACTOR"]
 message = os.environ["COMMIT_MESSAGE"]
-short_commit = os.environ["SHA7"]
+short_commit = os.environ["GITHUB_SHA"][:7]
 target_chat_ids = json.loads(os.environ["TARGET_CHAT_IDS"])
 
 print(f"Sending '{artifact}' to the following chat IDs: {target_chat_ids}")
 print(f"Issued by user {actor}")
 print(f"With commit message '{message}'")
 
-caption = f"New MMS release:\n\n" \
+caption = f"*New MMS release*\n\n" \
           f"'{message}'\n\n" \
           f"Issue by `{actor}`\n" \
           f"Short commit: `{short_commit}`"
