b05b66a9ddd273caedce53e2fe558a8c69dcf50b
Kaikyu <kaikyu4k@gmail.com>
2020-01-25 22:40:13 +0100

    Some other small improvements to Github CI

--- .github/workflows/deployer.py
+++ .github/workflows/deployer.py
@@ -6,12 +6,21 @@ import json
 from telegram import Bot
 
 token = os.environ["BOT_TOKEN"]
-target_chat_ids = json.loads(os.environ["TARGET_CHAT_IDS"])
+artifact = os.environ["ARTIFACT"]
 actor = os.environ["GITHUB_ACTOR"]
 message = os.environ["COMMIT_MESSAGE"]
+short_commit = os.environ["SHA7"]
+target_chat_ids = json.loads(os.environ["TARGET_CHAT_IDS"])
 
-print(f"Sending to the following chat IDs: {target_chat_ids}")
+print(f"Sending '{artifact}' to the following chat IDs: {target_chat_ids}")
+print(f"Issued by user {actor}")
+print(f"With commit message '{message}'")
+
+caption = f"New MMS release:\n\n" \
+          f"'{message}'\n\n" \
+          f"Issue by `{actor}`\n" \
+          f"Short commit: `{short_commit}`"
 
 bot = Bot(token)
 for target_chat_id in target_chat_ids:
-    bot.send_document(target_chat_id, open('target/moe-moe-secretary.jar', 'rb'))
+    bot.send_document(target_chat_id, open(artifact, 'rb'), caption=caption, parse_mode="markdown")
